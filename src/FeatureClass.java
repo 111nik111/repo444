577bc6a18ebcf8a6bacce099dabcf183ae13e885
nikolasavic <nikolasav@gmail.com>
2016-10-29 12:01:37 +0200

    change2

--- src/FeatureClass.java
+++ src/FeatureClass.java
@@ -2,7 +2,12 @@
  * @author nikolasavic
  */
 public class FeatureClass {
-    public void doSomeFeature(){
-        String str="Some great feature";
+    public void doSomeFeature() {
+        String str = "Some great feature";
     }
+
+
+    public void secondChanges() {
+        String str = "This is second changes!";
+
 }
